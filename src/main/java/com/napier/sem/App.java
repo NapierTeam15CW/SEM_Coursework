cd75b853a395dc26a2675a231c6161ea65f63423
Graeme White <40415739@live.napier.ac.uk>
2019-01-22 18:45:37 +0000

    Second commit - Addition of Java file to project

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Group 15 Coursework");
+        System.out.println("Group 15 Coursework is a go go!");
     }
 }
