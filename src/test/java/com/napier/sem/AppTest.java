cfe1139f35412024e22fff2988132ae6bcdb1f62
Stephen Weatherhead <stephenw42@outlook.com>
2019-03-07 13:47:33 +0000

    displayCountriesTestEmpty test added

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.util.ArrayList;
+
 public class AppTest {
 
     static App app;
@@ -18,5 +20,12 @@ public class AppTest {
     void displayCountriesTestNull()
     {
         app.displayCountries(null);
+    }
+
+    @Test
+    void displayCountriesTestEmpty()
+    {
+        ArrayList<Country> countries = new ArrayList<>();
+        app.displayCountries(countries);
     }
 }
