5b935fbce2f10d57b7fd9cc545b16338479a61da
Graeme White <40415739@live.napier.ac.uk>
2019-03-10 16:18:20 +0000

    Added unit tests for city report methods

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -54,4 +54,100 @@ public class AppTest {
         // Display country list
         app.displayCountries(countries);
     }
+
+    /**
+     * Method tests the displayCities method
+     * by using a null value instead of a
+     * list
+     */
+    @Test
+    void displayCitiesNull()
+    {
+        // Invoke displayCities method
+        // with null value
+        app.displayCities(null);
+    }
+
+    /**
+     * Method tests the displayCities method
+     * by using a city list with no entries
+     */
+    @Test
+    void displayCitiesEmptyList()
+    {
+        // Creating city list
+        ArrayList<City> cities = new ArrayList<>();
+
+        // Invoking displayCity method with no
+        // values added to list
+        app.displayCities(cities);
+    }
+
+    /**
+     * Method tests the displayCities method
+     * by using a city list that only has
+     * a null entry
+     */
+    @Test
+    void displayCitiesWithNullCity()
+    {
+        // Creating new city list
+        ArrayList<City> cities = new ArrayList<>();
+
+        // Adding null value to list
+        cities.add(null);
+
+        // Invoking displayCities method
+        // with null value added to city
+        // list
+        app.displayCities(cities);
+    }
+
+    /**
+     * Method tests the displayCities method
+     * by using a list of cities with
+     * test data
+     */
+    @Test
+    void displayCitiesWithValues()
+    {
+        // Creating new city list
+        ArrayList<City> cities = new ArrayList<>();
+
+        // Creating 4 test cities
+        City testCity1 = new City();
+        City testCity2 = new City();
+        City testCity3 = new City();
+        City testCity4 = new City();
+
+        // Adding values to each test city (Spot the games dev student)
+        testCity1.Name = "Los Santos";
+        testCity1.District = "San Andreas";
+        testCity1.CountryName = "North America";
+        testCity1.Population = 4100300;
+
+        testCity2.Name = "City 17";
+        testCity2.District = "Eastern Europe";
+        testCity2.CountryName = "Bulgaria";
+        testCity2.Population = 333333;
+
+        testCity3.Name = "Racoon City";
+        testCity3.District = "Missouri";
+        testCity3.CountryName = "North America";
+        testCity3.Population = 1234321;
+
+        testCity4.Name = "Midgar";
+        testCity4.District = "Eastern Europe";
+        testCity4.CountryName = "Bulgaria";
+        testCity4.Population = 541234;
+
+        // Adding test cities to list
+        cities.add(testCity1);
+        cities.add(testCity2);
+        cities.add(testCity3);
+        cities.add(testCity4);
+
+        // Invoking displayCities method with city list
+        app.displayCities(cities);
+    }
 }
