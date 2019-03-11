b6b4bb54303155d70d9b4d17aff3c6fe3623df44
ArranSmedley <arransmedleywork@gmail.com>
2019-03-11 12:27:54 +0000

    Added App Integration Test Class

---
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppIntegrationTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect();
+    }
+
+    @Test
+    void testGetCity()
+    {
+        City city = app.getCity(1);
+        assertEquals(city.ID, 1);
+        assertEquals(city.Name, "Kabul");
+        assertEquals(city.CountryCode, "AFG");
+    }
+}
